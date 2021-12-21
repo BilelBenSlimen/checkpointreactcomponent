27c958eeb14affaee80ddb6fb1e5148eaf6c987f
Abdellaoui Bilel <zendesignstudioarc@gmail.com>
2021-12-21 14:05:04 +0100

    first commit

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Address() {
+    return (
+        <div>
+            <h2>Tunis Ariana Ennasr 2</h2>
+            
+        </div>
+    )
+}
+
+export default Address
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ProfilePhoto() {
+    return (
+        <div>
+            <img src="https://cdn1.vectorstock.com/i/1000x1000/31/95/user-sign-icon-person-symbol-human-avatar-vector-12693195.jpg" alt='myphoto' />
+
+            
+        </div>
+    )
+}
+
+export default ProfilePhoto
